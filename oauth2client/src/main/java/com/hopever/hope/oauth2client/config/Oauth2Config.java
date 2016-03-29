31197ddb1f31a5685dbc473ca6e8ee5449050aaa
mrhop <233402923@qq.com>
2016-03-29 17:34:01 +0800

    just oauth2

--- oauth2client/src/main/java/com/hopever/hope/oauth2client/config/Oauth2Config.java
+++ oauth2client/src/main/java/com/hopever/hope/oauth2client/config/Oauth2Config.java
@@ -9,9 +9,12 @@ import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.security.oauth2.client.DefaultOAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.oauth2.client.token.AccessTokenRequest;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeAccessTokenProvider;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.client.token.grant.implicit.ImplicitResourceDetails;
+import org.springframework.security.oauth2.client.token.grant.password.ResourceOwnerPasswordResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 import javax.annotation.Resource;
@@ -41,7 +44,13 @@ public class Oauth2Config {
 
 
         @Bean
-        public OAuth2ProtectedResourceDetails authorWithAuthorizationCode() {
+        @Qualifier("authorizationCodeAccessTokenProvider")
+        public AuthorizationCodeAccessTokenProvider authorizationCodeAccessTokenProvider() {
+            return new AuthorizationCodeAccessTokenProvider();
+        }
+
+        @Bean
+        public AuthorizationCodeResourceDetails authorWithAuthorizationCode() {
             AuthorizationCodeResourceDetails details = new AuthorizationCodeResourceDetails();
             details.setId("staff_test");
             details.setClientId("rajithapp2");
@@ -67,6 +76,83 @@ public class Oauth2Config {
         public OAuth2RestTemplate authorWithAuthorizationCodeTemplate() {
             return new OAuth2RestTemplate(authorWithAuthorizationCode(), authorWithAuthorizationCodeClientContext());
         }
+
+        @Bean
+        public ResourceOwnerPasswordResourceDetails authorWithPassword() {
+            ResourceOwnerPasswordResourceDetails details = new ResourceOwnerPasswordResourceDetails();
+            details.setId("staff_test");
+            details.setClientId("rajithapp");
+            details.setClientSecret("secret");
+            details.setUsername("admin");
+            details.setPassword("admin");
+            details.setAccessTokenUri(accessTokenUri);
+            details.setScope(Arrays.asList("read", "write"));
+            return details;
+        }
+
+        @Bean
+        @Qualifier("authorWithPasswordClientContext")
+        @Scope(value = "session", proxyMode = ScopedProxyMode.INTERFACES)
+        public OAuth2ClientContext authorWithPasswordClientContext() {
+            return new DefaultOAuth2ClientContext(accessTokenRequest);
+        }
+
+        @Bean
+        @Qualifier("authorWithPasswordTemplate")
+        public OAuth2RestTemplate authorWithPasswordTemplate() {
+            return new OAuth2RestTemplate(authorWithPassword(), authorWithAuthorizationCodeClientContext());
+        }
+
+        @Bean
+        public ClientCredentialsResourceDetails authorWithClient() {
+            ClientCredentialsResourceDetails details = new ClientCredentialsResourceDetails();
+            details.setId("staff_test");
+            details.setClientId("rajithapp2");
+            details.setClientSecret("secret");
+            details.setAccessTokenUri(accessTokenUri);
+            details.setScope(Arrays.asList("read", "write"));
+            return details;
+        }
+
+        @Bean
+        @Qualifier("authorWithClientClientContext")
+        @Scope(value = "session", proxyMode = ScopedProxyMode.INTERFACES)
+        public OAuth2ClientContext authorWithClientClientContext() {
+            return new DefaultOAuth2ClientContext(accessTokenRequest);
+        }
+
+        @Bean
+        @Qualifier("authorWithClientTemplate")
+        public OAuth2RestTemplate authorWithClientTemplate() {
+            return new OAuth2RestTemplate(authorWithClient(), authorWithClientClientContext());
+        }
+
+
+        @Bean
+        public ImplicitResourceDetails authorWithImplicit() {
+            ImplicitResourceDetails details = new ImplicitResourceDetails();
+            details.setId("staff_test");
+            details.setClientId("rajithapp3");
+            details.setClientSecret("secret");
+            details.setAccessTokenUri(accessTokenUri);
+            details.setUserAuthorizationUri(userAuthorizationUri);
+            details.setScope(Arrays.asList("read", "write"));
+            return details;
+        }
+
+        @Bean
+        @Qualifier("authorWithImplicitClientContext")
+        @Scope(value = "session", proxyMode = ScopedProxyMode.INTERFACES)
+        public OAuth2ClientContext authorWithImplicitClientContext() {
+            return new DefaultOAuth2ClientContext(accessTokenRequest);
+        }
+
+        @Bean
+        @Qualifier("authorWithImplicitTemplate")
+        public OAuth2RestTemplate authorWithImplicitTemplate() {
+            return new OAuth2RestTemplate(authorWithImplicit(), authorWithImplicitClientContext());
+        }
+
 
     }
 }
